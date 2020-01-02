12b4916f57d41de1a608eaa2a147fae9e12fc590
Timmy Jose <zoltan.jose@gmail.com>
2020-01-02 18:31:09 +0530

    rustlings - day 1

--- exercises/test4.rs
+++ exercises/test4.rs
@@ -5,10 +5,17 @@
 
 // Write a macro that passes the test! No hints this time, you can do it!
 
-// I AM NOT DONE
-
 fn main() {
     if my_macro!("world!") != "Hello world!" {
         panic!("Oh no! Wrong output!");
+    }
+}
+
+mod custom_macros {
+    #[macro_export]
+    macro_rules! my_macro {
+        ($string: expr) => {
+            "Hello world"
+        }
     }
 }
